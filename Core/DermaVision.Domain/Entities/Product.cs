02d00222296cdc7c14452955baff5dbfca026a9c
1EMRE0 <ovacike7@gmail.com>
2025-10-17 20:40:44 +0300

    Add product class and UpdateStock method

---
+++ Core/DermaVision.Domain/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Domain.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; } 
+        public string ProductImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; } 
+        public void UpdateStcok(int amount)
+        {
+            if(amount < 0)
+            {
+                throw new ArgumentException("Stock amount cannot be negative");
+            }
+            ProductStock = amount;
+        } // stok miktarı girişinin kontrolü. bu metod admin controller kısmında çağrılacak.
+
+    }
+
+    
+}
