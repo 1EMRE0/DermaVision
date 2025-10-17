ae65dae6dba3b14e0ae76a16909189572bddd8bb
1EMRE0 <ovacike7@gmail.com>
2025-10-17 21:45:34 +0300

    Add DermaVision.Persistance and nugget package

---
+++ Infrastructure/DermaVision.Persistence/Context/DermaVisionContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Persistence.Context
+{
+    public class DermaVisionContext
+    {
+
+    }
+}
+
+
+//Infrastructure / Persistence → Application ve Domain’in interface’lerini uygular
