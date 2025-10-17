58b8e38f0ad12e0aa2af3a57d0e11f6188590487
1EMRE0 <ovacike7@gmail.com>
2025-10-17 22:08:12 +0300

    Add dermavision context dbset

--- Infrastructure/DermaVision.Persistence/Context/DermaVisionContext.cs
+++ Infrastructure/DermaVision.Persistence/Context/DermaVisionContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DermaVision.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +8,22 @@ using System.Threading.Tasks;
 
 namespace DermaVision.Persistence.Context
 {
-    public class DermaVisionContext
+    public class DermaVisionContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost; initial Catalog=DermaVisionDb;integrated Security=true;");
+        }
 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
     }
 }
 
 
 //Infrastructure / Persistence → Application ve Domain’in interface’lerini uygular
+
+//protected sayesinde dışarıdan doğrudan çağrılamaz.
+//
