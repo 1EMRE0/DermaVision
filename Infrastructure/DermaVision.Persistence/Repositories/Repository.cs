b00a82f3973a1a70275c23c45e9192be27836e53
1EMRE0 <ovacike7@gmail.com>
2025-10-18 17:09:24 +0300

    Inject DermaVisionContext into Repository<T> for EF Core operations

---
+++ Infrastructure/DermaVision.Persistence/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using DermaVision.Application.Interfaces;
+using DermaVision.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DermaVisionContext _context;
+        public Task CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
