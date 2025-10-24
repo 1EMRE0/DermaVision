aeef0b8a6d485999f5697779b9952cb9784dd6db
1EMRE0 <ovacike7@gmail.com>
2025-10-24 08:57:47 +0300

    Add: categoryquery class

---
+++ Core/DermaVision.Application/Features/CQRS/Results/Category/GetCategoryQueryResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Results.Category
+{
+    public class GetCategoryQueryResult
+    {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+    }
+}
+//Dışarıya dönecek veri modellerini içerir.
+// sınıf o
