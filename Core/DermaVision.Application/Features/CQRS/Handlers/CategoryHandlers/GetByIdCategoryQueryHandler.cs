b6a6a9a80606303dd3e11d2a68f9889388002f86
1EMRE0 <ovacike7@gmail.com>
2025-10-26 00:02:32 +0300

    feat(category): add GetByIdCategoryQueryHandler to retrieve a category by ID

---
+++ Core/DermaVision.Application/Features/CQRS/Handlers/CategoryHandlers/GetByIdCategoryQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using DermaVision.Application.Features.CQRS.Quaries.CategoryQuaries;
+using DermaVision.Application.Features.CQRS.Results.Category;
+using DermaVision.Application.Interfaces;
+using DermaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class GetByIdCategoryQueryHandler
+    {
+        private readonly IRepository<Category> _repository;
+        public GetByIdCategoryQueryHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetCategoryQueryByIdResult> Handle(GetCategoryByIdQuary query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetCategoryQueryByIdResult
+            {
+                CategoryId = values.CategoryId,
+                Name = values.Name
+            };
+                
+            
+                
+        }
+    }
+}
