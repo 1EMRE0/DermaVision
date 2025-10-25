64fdc584f2025b561da5b4bd31ef542eb64bb884
1EMRE0 <ovacike7@gmail.com>
2025-10-26 00:07:47 +0300

    feat(category): add RemoveCategoryCommand for delete operation

---
+++ Core/DermaVision.Application/Features/CQRS/Commands/CategoryCommands/CreateCategoryCommand.cs
@@ -0,0 +1,16 @@
+﻿using DermaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Commands.CategoryCommands
+{
+    public class CreateCategoryCommand
+    {
+        //public int CategoryId { get; set; } categoryid kullanmıyorum
+        public string? Name { get; set; }
+
+    }
+}
---
+++ Core/DermaVision.Application/Features/CQRS/Commands/CategoryCommands/RemoveCategoryCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Commands.CategoryCommands
+{
+    public class RemoveCategoryCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveCategoryCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
+//amaç , Silinecek kategorinin ID’sini taşımak
