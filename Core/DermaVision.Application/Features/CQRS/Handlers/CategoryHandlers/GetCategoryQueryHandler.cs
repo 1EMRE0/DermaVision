962220f13e6febfa33ce2fbfc2652e362b0e03a6
1EMRE0 <ovacike7@gmail.com>
2025-10-25 22:51:50 +0300

    feat(category): implement GetCategoryQueryHandler to return category list with mapping to result model

---
+++ Core/DermaVision.Application/Features/CQRS/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using DermaVision.Application.Features.CQRS.Results.Category;
+using DermaVision.Application.Interfaces;
+using DermaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class GetCategoryQueryHandler
+    {
+        private readonly IRepository<Category> _repository;
+
+        public GetCategoryQueryHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetCategoryQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetCategoryQueryResult
+            {
+                CategoryId = x.CategoryId,
+                Name = x.Name
+            }).ToList();
+        }
+    }
+}
+//crud işlemleri
---
+++ Core/DermaVision.Application/Features/CQRS/Quaries/CategoryQuaries/GetCategoryByIdQuary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Quaries.CategoryQuaries
+{
+    public class GetCategoryByIdQuary
+    {
+        public GetCategoryByIdQuary(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
