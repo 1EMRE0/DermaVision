e6b35ebea0af66b851bf0e5dd93ca2d8738ae3c7
1EMRE0 <ovacike7@gmail.com>
2025-10-28 02:15:00 +0300

    feat(Category): Kategori Yönetimi için CQRS tabanlı API Kontrolcüsü eklendi

--- Presentation/DermaVision.WebApi/Controllers/CategoriesController.cs
+++ Presentation/DermaVision.WebApi/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers;
+﻿using DermaVision.Application.Features.CQRS.Commands.CategoryCommands;
+using DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers;
+using DermaVision.Application.Features.CQRS.Quaries.CategoryQuaries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +23,41 @@ namespace DermaVision.WebApi.Controllers
             _getCategoryQueryHandler = getCategoryQueryHandler;
             _RemoveCategoryCommandHandler = removeCategoryCommandHandler;
             _UpdateCategoryCommandHandler = updateCategoryCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var value = await _getCategoryQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategory(int id)
+        {
+            var value = await _getByIdCategoryQueryHandler.Handle(new GetCategoryByIdQuary(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+        {
+            await _createCategoryCommandHandler.Handle(command);
+            return Ok("Category added.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            await _RemoveCategoryCommandHandler.Handle(new RemoveCategoryCommand(id));
+            return Ok("Category deleted.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryCommand command)
+        {
+            await _UpdateCategoryCommandHandler.Handle(command);
+            return Ok("Category updated.");
         }
     }
 }
