9197c00af461c3a95a8ec05e1617f6ad18e3a346
1EMRE0 <ovacike7@gmail.com>
2025-10-28 00:27:40 +0300

    feat(API): add CategoriesController with injected CQRS handlers

---
+++ Presentation/DermaVision.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DermaVision.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly GetByIdCategoryQueryHandler _getByIdCategoryQueryHandler;
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly RemoveCategoryCommandHandler _RemoveCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _UpdateCategoryCommandHandler;
+
+        public CategoriesController(CreateCategoryCommandHandler createCategoryCommandHandler, GetByIdCategoryQueryHandler getByIdCategoryQueryHandler, GetCategoryQueryHandler getCategoryQueryHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler)
+        {
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+            _getByIdCategoryQueryHandler = getByIdCategoryQueryHandler;
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            _RemoveCategoryCommandHandler = removeCategoryCommandHandler;
+            _UpdateCategoryCommandHandler = updateCategoryCommandHandler;
+        }
+    }
+}
