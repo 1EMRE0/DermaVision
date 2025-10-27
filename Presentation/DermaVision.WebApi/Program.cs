a3edfa1972c6f4c22a3f4a8ae330b7aff819b416
1EMRE0 <ovacike7@gmail.com>
2025-10-28 02:21:41 +0300

    refactor(DI): Uygulama Katmanı Handler Bağımlılıkları Kaydedildi

--- Presentation/DermaVision.WebApi/Program.cs
+++ Presentation/DermaVision.WebApi/Program.cs
@@ -1,6 +1,17 @@
+using DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers;
+using DermaVision.Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<DermaVisionContext>();
+builder.Services.AddScoped<GetCategoryQueryHandler>();
+builder.Services.AddScoped<GetByIdCategoryQueryHandler>();
+builder.Services.AddScoped<CreateCategoryCommandHandler>();
+builder.Services.AddScoped<UpdateCategoryCommandHandler>();
+builder.Services.AddScoped<RemoveCategoryCommandHandler>();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
