dd6b1d069add2659d4343c82adcb27a4021cbbc4
1EMRE0 <ovacike7@gmail.com>
2025-10-28 00:01:41 +0300

    feat(CQRS): add RemoveCategoryCommandHandler for category deletion

---
+++ Core/DermaVision.Application/Features/CQRS/Handlers/CategoryHandlers/CreateCategoryCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using DermaVision.Application.Features.CQRS.Commands.CategoryCommands;
+using DermaVision.Application.Interfaces;
+using DermaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class CreateCategoryCommandHandler
+    {
+        private readonly IRepository<Category> _repository;
+
+        public CreateCategoryCommandHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateCategoryCommand command)
+        {
+            await _repository.CreateAsync(new Category
+            {
+                Name = command.Name
+            });
+        }
+    }
+}
---
+++ Core/DermaVision.Application/Features/CQRS/Handlers/CategoryHandlers/RemoveCategoryCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using DermaVision.Application.Features.CQRS.Commands.CategoryCommands;
+using DermaVision.Application.Interfaces;
+using DermaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class RemoveCategoryCommandHandler
+    {
+        private readonly IRepository<Category> _repository;
+
+        public RemoveCategoryCommandHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveCategoryCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.Id);
+            await _repository.RemoveAsync(value);
+        }
+    }
+}
