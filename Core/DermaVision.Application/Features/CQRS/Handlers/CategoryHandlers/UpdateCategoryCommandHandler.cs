9c1edbf4be69aeb0092e095e79a450f54387788f
1EMRE0 <ovacike7@gmail.com>
2025-10-28 00:14:08 +0300

    feat(CQRS): add UpdateCategoryCommandHandler for category updates

---
+++ Core/DermaVision.Application/Features/CQRS/Handlers/CategoryHandlers/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using DermaVision.Application.Features.CQRS.Commands.CategoryCommands;
+using DermaVision.Application.Interfaces;
+using DermaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class UpdateCategoryCommandHandler
+    {
+        private readonly IRepository<Category> _repository;
+
+        public UpdateCategoryCommandHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateCategoryCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.CategoryId);
+            value.CategoryId = command.CategoryId;
+            value.Name = command.Name;
+
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
