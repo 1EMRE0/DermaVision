e65b6ca00cdcf3e1184a14f7a694156f3422b19c
1EMRE0 <ovacike7@gmail.com>
2025-11-03 00:10:40 +0300

    getproductbyidqueryhandler eklendi

---
+++ Core/DermaVision.Application/Features/CQRS/Handlers/ProdctHandlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using DermaVision.Application.Features.CQRS.Quaries.ProductQuries;
+using DermaVision.Application.Features.CQRS.Results.ProductResult;
+using DermaVision.Application.Interfaces;
+using DermaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DermaVision.Domain.Entities;
+
+namespace DermaVision.Application.Features.CQRS.Handlers.ProdctHandlers
+{
+    public class GetProductByIdQueryHandler
+    {
+        private readonly IRepository<Product> _repository;
+
+        public GetProductByIdQueryHandler(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetProductQueryByIdResult> Handle(GetProductByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetProductQueryByIdResult
+            {
+                CategoryId = values.CategoryId,
+                ProductDescription = values.ProductDescription,
+                ProductId = values.ProductId,
+                ProductImageUrl = values.ProductImageUrl,
+                ProductName = values.ProductName,
+                ProductPrice = values.ProductPrice,
+                ProductStock = values.ProductStock,
+                SkinType = values.SkinType
+            };
+        }
+    }
+}
---
+++ Core/DermaVision.Application/Features/CQRS/Quaries/ProductQuries/GetProductByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DermaVision.Application.Features.CQRS.Quaries.ProductQuries
+{
+    public class GetProductByIdQuery
+    {
+        public int Id { get; set; }
+
+        public GetProductByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
